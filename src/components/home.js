bc0ffbccf71a6d661ffdf41934ee12f47ec89259
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-09-18 10:47:11 -0500

    first build class component

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Header from './header';
+import Card from './card';
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         {Header()}
+        <Card />
 
 
       </div>
---
+++ src/components/input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Input = (title) => {
+    return (
+        <div class="input">
+            <input />
+            <label>{title}</label>
+        </div>
+    )
+}
+export default Input;
