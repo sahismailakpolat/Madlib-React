99b45d3e861e697139b2d52cca8a443d8cac92ed
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-09-18 10:23:19 -0500

    build first functional component

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div class="header">
+            <h1>Bottega Madlibs</h1>
+            <p>Fill out the fields below and click</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from './header';
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        {Header()}
+
+
+      </div>
+    );
+  }
+}
