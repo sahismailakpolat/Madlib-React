96c0d95e174f74847f1a918eeeedb2243bb1e40e
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-09-19 23:03:42 -0500

    adding border to the header

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div class="header">
+            <div className="header__skew"></div>
+            <div className="header__border"></div>
             <h1>Bottega Madlibs</h1>
             <p>Fill out the fields below and click</p>
         </div>
