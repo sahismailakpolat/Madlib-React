c16213164fa343594eaf7736669e3fc21c78fb42
akpolat.sahismail@gmail.com <akpolat.sahismail@gmail.com>
2018-10-02 10:28:51 -0500

    changed the title

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div class="header">
             <div className="header__skew"></div>
             <div className="header__border"></div>
-            <h1>Bottega Madlibs</h1>
+            <h1>Madlibs Project</h1>
             <p>Fill out the fields below and click the generate button to see the Mad Lib story</p>
         </div>
     )
